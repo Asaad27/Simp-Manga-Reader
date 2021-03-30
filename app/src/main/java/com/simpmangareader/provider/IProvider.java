459e400a8e2d1f1d807a79191d6519a9f9da024a
Fakhri Moud <moad_fakhri_1999@outlook.com>
2021-03-30 22:34:17 +0100

    add methode to get chapters detail from a manga

--- app/src/main/java/com/simpmangareader/provider/IProvider.java
+++ app/src/main/java/com/simpmangareader/provider/IProvider.java
@@ -4,5 +4,11 @@ package com.simpmangareader.provider;
 * interface for manga providers
 */
 
+import com.simpmangareader.provider.data.ChapterDetail;
+import com.simpmangareader.provider.data.MangaDetail;
+
+import java.util.Collection;
+
 public interface IProvider {
+    Collection<ChapterDetail> GetChaptersDetail(MangaDetail manga);
 }
