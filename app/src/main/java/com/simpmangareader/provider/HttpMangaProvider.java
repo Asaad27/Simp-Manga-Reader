a9264b56198a0540c1eb69156889519d6c98145d
Fakhri Moud <moad_fakhri_1999@outlook.com>
2021-03-30 22:35:03 +0100

    add some basic methods

--- app/src/main/java/com/simpmangareader/provider/HttpMangaProvider.java
+++ app/src/main/java/com/simpmangareader/provider/HttpMangaProvider.java
@@ -1,4 +1,15 @@
 package com.simpmangareader.provider;
 
+
+import com.simpmangareader.provider.data.MangaDetail;
+
+import java.util.Collection;
+
 public abstract class HttpMangaProvider implements IProvider {
+    String baseURL;
+    String name;
+
+    abstract public Collection<MangaDetail> GetPopularManga(int pageNumber);
+    abstract public Collection<MangaDetail> GetMangaList(int pageNumber);
+    abstract public Collection<MangaDetail> SearchForManga(int pageNumber, String query);
 }
