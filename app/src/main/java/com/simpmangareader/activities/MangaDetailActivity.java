879131351218565033cc8a492ba8ba82afd37a7a
fakhri Moad <moad_fakhri_1999@outlook.com>
2021-05-31 16:20:18 +0100

    display manga detail in MangaDetailActivity

--- app/src/main/java/com/simpmangareader/activities/MangaDetailActivity.java
+++ app/src/main/java/com/simpmangareader/activities/MangaDetailActivity.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.simpmangareader.R;
@@ -33,6 +35,11 @@ public class MangaDetailActivity extends AppCompatActivity {
     private static final String TAG = "RecyclerViewFragment";
     private static final String KEY_LAYOUT_MANAGER = "layoutManager";
     private static final int COLUMN_WIDTH = 130;
+    private ImageView coverImage;
+    private TextView titleText;
+    private TextView categoryText;
+    private TextView statusText;
+    private TextView descriptionText;
     Manga manga;
     Chapter[] chapters;
 
@@ -49,9 +56,22 @@ public class MangaDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manga_detail);
+        coverImage = findViewById(R.id.imageView);
+        titleText = findViewById(R.id.manga_detail_title_tv);
+        categoryText = findViewById(R.id.manga_category_title_tv);
+        statusText = findViewById(R.id.manga_detail_status_tv);
+        descriptionText = findViewById(R.id.manga_detail_description_tv);
+
         chapters = new Chapter[0];
         //get data from previous activity
         manga =  this.getIntent().getExtras().getParcelable("manga");
+
+        coverImage.setImageBitmap(manga.cover);
+        titleText.setText(manga.title);
+        categoryText.setText(manga.publicationDemographic);
+        statusText.setText(manga.status);
+        descriptionText.setText(manga.description);
+
         //TODO: use manga to set the manga specific data
         Mangadex.FetchAllMangaEnglishChapter(manga.id, result -> {
             //TODO: display the fetched chapters
