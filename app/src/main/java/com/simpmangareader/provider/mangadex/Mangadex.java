134885b56f362882af4f9c8085f9d3fabf0aea9f
fakhri Moad <moad_fakhri_1999@outlook.com>
2021-05-31 01:17:18 +0100

    fetch english chapters of a manga

--- app/src/main/java/com/simpmangareader/provider/mangadex/Mangadex.java
+++ app/src/main/java/com/simpmangareader/provider/mangadex/Mangadex.java
@@ -3,10 +3,11 @@ package com.simpmangareader.provider.mangadex;
 import android.content.Context;
 import android.os.Handler;
 import android.util.Log;
-import android.view.View;
 
-import com.simpmangareader.callbacks.NetworkAllMangaFetchFailed;
+import com.simpmangareader.callbacks.NetworkFailed;
 import com.simpmangareader.callbacks.NetworkAllMangaFetchSucceed;
+import com.simpmangareader.callbacks.NetworkMangaChaptersSucceed;
+import com.simpmangareader.provider.data.Chapter;
 import com.simpmangareader.provider.data.Manga;
 
 import org.json.JSONArray;
@@ -15,7 +16,6 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.*;
 
@@ -41,7 +41,7 @@ public class Mangadex
 	**/
 	static public void FetchManga(int offset, int limit,
 								  final NetworkAllMangaFetchSucceed successCallback,
-								  final NetworkAllMangaFetchFailed failedCallback,
+								  final NetworkFailed failedCallback,
 								  final Handler handler)
 	{
 		if (limit <= 0) limit = 1;
@@ -58,7 +58,7 @@ public class Mangadex
 
 	static public void SearchMangaByName(int offset, int limit, String name,
 										 final NetworkAllMangaFetchSucceed successCallback,
-										 final NetworkAllMangaFetchFailed failedCallback,
+										 final NetworkFailed failedCallback,
 										 final Handler handler)
 	{
 		if (limit <= 0) limit = 1;
@@ -73,7 +73,7 @@ public class Mangadex
 		});
 	}
 
-	private static void GetMangaByURL(NetworkAllMangaFetchSucceed successCallback, NetworkAllMangaFetchFailed failedCallback, Handler handler, String reqURL) {
+	private static void GetMangaByURL(NetworkAllMangaFetchSucceed successCallback, NetworkFailed failedCallback, Handler handler, String reqURL) {
 		Log.e("TAG", "reqURL : " + reqURL);
 		try {
 			URL url = new URL(reqURL);
@@ -99,8 +99,8 @@ public class Mangadex
 	private static Manga[] ParseJsonToManga(String data) throws JSONException {
 		JSONObject json = new JSONObject(data);
 		//start parsing
-		int count = json.getInt("limit");
 		JSONArray array = json.getJSONArray("results");
+		int count = array.length();
 		Manga[] mangas = new Manga[count];
 		for (int i = 0; i < count; ++i)
 		{
@@ -121,6 +121,77 @@ public class Mangadex
 
 		}
 		return mangas;
+	}
+
+
+	public static void FetchAllMangaEnglishChapter(final String mangaID,
+												   final NetworkMangaChaptersSucceed successCallback,
+												   final NetworkFailed failedCallback,
+												   final Handler handler)
+	{
+		//TODO(Mouad): compress this stuff, ie factor the repeating parts into functions
+		executor.execute(()->{
+			int totalChaptersCount;
+			int offset = 0;
+			String reqURL = baseURL + "/chapter?manga="+mangaID+"&translatedLanguage[0]=en&limit=100";
+			try {
+				URL url = new URL(reqURL);
+				HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+				BufferedInputStream in = new BufferedInputStream(urlConnection.getInputStream());
+				String data = "";
+				//read data from the stream
+				byte[] contents = new byte[1024];
+				int bytesRead;
+				while ((bytesRead = in.read(contents)) != -1) {
+					data += new String(contents, 0, bytesRead);
+				}
+				JSONObject json = new JSONObject(data);
+				totalChaptersCount = json.getInt("total");
+				Chapter[] chapters = new Chapter[totalChaptersCount];
+				do {
+					JSONArray array = json.getJSONArray("results");
+					int fetched = array.length();
+					for (int i = 0; i < fetched; ++i)
+					{
+						JSONObject result = array.getJSONObject(i);
+						if (!result.getString("result").equals("ok")) continue;
+						JSONObject chapterJson = result.getJSONObject("data");
+						chapters[offset + i] = new Chapter();
+						chapters[offset + i].id = chapterJson.getString("id");
+						JSONObject chapterAttributeJson = chapterJson.getJSONObject("attributes");
+						chapters[offset + i].translatedLanguage = "en";
+						chapters[offset + i].volume = chapterAttributeJson.getString("volume");
+						chapters[offset + i].title = chapterAttributeJson.getString("title");
+						chapters[offset + i].chapterNumber = chapterAttributeJson.getString("chapter");
+						chapters[offset + i].hash = chapterAttributeJson.getString("hash");
+						JSONArray chapterData = chapterAttributeJson.getJSONArray("data");
+						int dataSize = chapterData.length();
+						chapters[offset + i].data = new String[dataSize];
+						for (int j = 0; j < dataSize; ++j)
+						{
+							chapters[offset + i].data[j] = chapterData.toString(j);
+						}
+					}
+					offset += fetched;
+					String nextURL = reqURL + "&offset="+offset;
+
+					url = new URL(nextURL);
+					urlConnection = (HttpsURLConnection) url.openConnection();
+					in = new BufferedInputStream(urlConnection.getInputStream());
+					data = "";
+					//read data from the stream
+					contents = new byte[1024];
+					while ((bytesRead = in.read(contents)) != -1) {
+						data += new String(contents, 0, bytesRead);
+					}
+					json = new JSONObject(data);
+				}
+				while(offset < totalChaptersCount);
+				handler.post(() -> successCallback.onComplete(chapters));
+			} catch (JSONException | IOException e) {
+				handler.post(() -> failedCallback.onError(e));
+			}
+		});
 	}
 
 }
